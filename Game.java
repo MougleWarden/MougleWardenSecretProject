10e5d5a24fb919a2e599ebcef81d83100f8b25db
The Warden <alex0216@outlook.com>
2017-04-22 14:21:58 -0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,36 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+	JPanel window = new JPanel();
+	JLabel playerLabel = new JLabel();
+	ImageIcon playerIcon = new ImageIcon("Pickleo.png");
+	
+	public static void main(String[] args)
+	{
+		new Game();
+		new World();
+		System.out.println("IT'S WORKING!!!"); //This executes after the starting code has finished to let you know that the game is working.
+	}
+	public Game()
+	{
+		setBackground(Color.BLACK);
+		setTitle("Test Window");
+		add(window);
+		setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		setUndecorated(true);
+		setVisible(true);
+		//window.setLayout(null);
+		//window.setSize(this.getSize());
+		//setLocationRelativeTo(null);
+		window.add(playerLabel);
+		playerLabel.setIcon(playerIcon);
+		//playerLabel.setSize(playerIcon.getIconWidth(), playerIcon.getIconHeight());
+		playerLabel.setLocation(window.getWidth() / 2 - playerLabel.getWidth() / 2, window.getHeight() / 2 - playerLabel.getHeight() / 2);
+		playerLabel.setVisible(true);
+	}
+}
